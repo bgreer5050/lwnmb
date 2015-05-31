639a23f25b31ed5dfb1f138d028551745f67c937
bgreer5050 <bgreer5050@gmail.com>
2015-05-31 13:10:21 -0500

    Add miles for music to home page

--- LWNMBSite2/Controllers/BoostersController.cs
+++ LWNMBSite2/Controllers/BoostersController.cs
@@ -24,7 +24,7 @@ namespace LWNMBSite2.Controllers
             return View();
         }
 
-        public ActionResult Committes()
+        public ActionResult Committees()
         {
             return View();
         }
