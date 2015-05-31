74bc41f6cce2e4fcb97a3c4a5e7eae42cae2fee1
bgreer5050 <bgreer5050@gmail.com>
2015-05-30 19:16:25 -0500

    NEXT Seperate Nav from Layout2

---
+++ LWNMBSite2/Controllers/BoostersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LWNMBSite2.Controllers
+{
+    public class BoostersController : Controller
+    {
+        // GET: Boosters
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Membership()
+        {
+            return View();
+        }
+
+        public ActionResult ExecutiveBoard()
+        {
+            return View();
+        }
+
+        public ActionResult Committes()
+        {
+            return View();
+        }
+
+        public ActionResult Constitution()
+        {
+            return View();
+        }
+
+        public ActionResult IBATransfer()
+        {
+            return View();
+        }
+
+        public ActionResult Meetings()
+        {
+            return View();
+        }
+
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+
+    }
+}
